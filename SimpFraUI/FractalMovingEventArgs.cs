a37113eb4d17d6ae37749aad633b518de4e4c2ff
Dmitry <demondima123456@gmail.com>
2014-04-10 21:50:54 +0600

    Moved FractalMovingEventArgs.cs

---
+++ SimpFraUI/FractalMovingEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace SimpFra
+{
+    public class FractalMovingEventArgs : EventArgs
+    {
+        SizeF shift;
+        PointF center;
+
+        public SizeF Shift
+        {
+            get
+            {
+                return shift;
+            }
+        }
+        public PointF Center
+        {
+            get
+            {
+                return center;
+            }
+        }
+    }
+}
