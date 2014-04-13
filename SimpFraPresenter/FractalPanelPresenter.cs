93a6ba1c3b3c762655ecd5af806c42167c7a18d3
Dmitry <demondima123456@gmail.com>
2014-04-13 18:33:56 +0600

    Added FractalPanelPresenter

---
+++ SimpFraPresenter/FractalPanelPresenter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRUC.Math;
+using SimpFraUI.Interfaces;
+using System.Drawing;
+
+namespace SimpFraPresenter
+{
+    public class FractalPanelPresenter
+    {
+        IFractalPanelView _view;
+        IFractalPanelView view
+        {
+            get
+            {
+                return _view;
+            }
+            set
+            {
+                _view = value;
+            }
+        }
+
+        public async void Render(Task<Bitmap> RenderTask)
+        {
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            view.FractalImage = await RenderTask;
+            sw.Stop();
+
+            _view.RenderTime = sw.Elapsed;
+        }
+    }
+}
---
+++ Tests/Presenters/FractalPanelPresenterTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using HRUC.Math;
+using Ninject;
+using NSubstitute;
+using NUnit.Core;
+using NUnit.Framework;
+using SimpFraPresenter;
+using SimpFraUI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Tests.Presenters
+{
+    [TestFixture]
+    class FractalPanelPresenterTests
+    {
+
+    }
+}
